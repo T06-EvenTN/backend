054b81c7859924e54c7f596e4ccfdeb376332381
DavideDaCol <118683352+DavideDaCol@users.noreply.github.com>
2024-10-27 17:28:24 +0100

    add json middleware and test post endpoint

--- app/index.js
+++ app/index.js
@@ -1,5 +1,9 @@
-const app = require('express')();
+const express = require('express');
+
+const app = express();
 const PORT = 8080;
+
+app.use(express.json());
 
 app.listen(
   PORT,
@@ -8,4 +12,17 @@ app.listen(
 
 app.get(`/test`, (req,res) => {
   res.status(200).send("yay!");
+})
+
+
+//TODO: actually implement this. this is just a test endpoint
+app.post(`/user`, (req,res) => {
+  const {id,email,userType} = req.body;
+  console.log(`creating user with id ${id}, email ${email} and admin privileges of ${userType}`);
+
+  if(Number(id)<1){
+    res.status(400).send({message: `an ID of ${id} is not valid`})
+  } else {
+    res.status(201).send(`${id}`);
+  }
 })
