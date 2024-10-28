efdd4a0feca21015db393c5c9cbdb566b54b71a0
DavideDaCol <118683352+DavideDaCol@users.noreply.github.com>
2024-10-28 16:02:16 +0100

    add database ping

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 
@@ -26,3 +26,23 @@ app.post(`/user`, (req,res) => {
     res.status(201).send(`${id}`);
   }
 })
+
+
+const mongoose = require('mongoose');
+const uri = process.env.DATABASE_URL;
+
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
+
