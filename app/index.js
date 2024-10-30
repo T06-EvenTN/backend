a1ed2a028fdc77ef037baac1b8ced58ba7db8aaf
DavideDaCol <118683352+DavideDaCol@users.noreply.github.com>
2024-10-30 14:06:03 +0100

    add app routing and database connection

--- app/index.js
+++ app/index.js
@@ -1,48 +1,33 @@
+//API requirements
 const express = require('express');
+
+const users = require('./users.js');
 
 const app = express();
 const PORT = process.env.PORT;
 
+//middleware definitions
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// mongoose connection
+const mongoose = require('mongoose');
+const uri = process.env.DATABASE_URL;
+
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+mongoose.connect(uri, clientOptions);
+
+//open HTTP connection on PORT
 app.listen(
   PORT,
   () => console.log(`application started on http://localhost:${PORT}`)
 );
 
+//request routing
 app.get(`/test`, (req,res) => {
   res.status(200).send("yay!");
 })
 
+app.use('/users', users);
 
-//TODO: actually implement this. this is just a test endpoint
-app.post(`/user`, (req,res) => {
-  const {id,email,userType} = req.body;
-  console.log(`creating user with id ${id}, email ${email} and admin privileges of ${userType}`);
-
-  if(Number(id)<1){
-    res.status(400).send({message: `an ID of ${id} is not valid`})
-  } else {
-    res.status(201).send(`${id}`);
-  }
-})
-
-
-const mongoose = require('mongoose');
-const uri = process.env.DATABASE_URL;
-
-const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
-
-async function run() {
-  try {
-    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-    await mongoose.connect(uri, clientOptions);
-    await mongoose.connection.db.admin().command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await mongoose.disconnect();
-  }
-}
-run().catch(console.dir);
-
+module.exports = app;
