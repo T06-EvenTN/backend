7378f96c6f613848ca546f9ac34f28960f373556
DavideDaCol <118683352+DavideDaCol@users.noreply.github.com>
2024-11-04 19:37:08 +0100

    add user deletion endpoint

--- app/users.js
+++ app/users.js
@@ -40,6 +40,21 @@ APIRouter.post('', async (req,res) => {
   let userid = user.id;
   res.status(201).send(`created user ${userid}`);
   //TODO: handle incorrect requests (400) and server errors 
+});
+
+APIRouter.delete('/:id', async (req,res) => {
+  try{
+    const { id } = req.params;
+    if(mongoose.isValidObjectId(id)){
+      const validReq = await User.findById(id);
+      if(validReq){
+        const deletion = await User.findByIdAndDelete(id);
+        res.status(200).send(`deleted user ${id}`);
+      } else res.status(404).send({message: "user not found"});
+    } else res.status(400).send({message: "invalid ID"});
+  } catch (error) {
+    res.status(500).send({message: `internal error: ${error}`});
+  }
 })
 
 //TODO: capire a cosa serve lol
