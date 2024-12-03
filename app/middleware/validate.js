334c98a00f0e71070b5cd24d0f51fb3ab7ed7e4b
Mattia Pistollato <mattia.pistollato@gmail.com>
2024-12-03 09:19:21 +0100

    fixed validate cause it only returned the last error

--- app/middleware/validate.js
+++ app/middleware/validate.js
@@ -4,7 +4,13 @@ function Validate(req, res, next) {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         let error = {};
-        errors.array().map((err) => (error[err.param] = err.msg));
+        errors.array().forEach(err => {  
+            if (!error[err.param]) {
+                error[err.param] = []; // Inizializza un array se non esiste
+            }
+            error[err.param].push(err.msg);
+        });
+        console.log(error);
         return res.status(422).json({ error });
     }
     next();
