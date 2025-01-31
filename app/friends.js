c1a132ab95bb234799b8ca9783820b029f546527
DavideDaCol <118683352+DavideDaCol@users.noreply.github.com>
2025-01-31 22:47:36 +0100

    add friend removal endpoint

--- app/friends.js
+++ app/friends.js
@@ -26,7 +26,7 @@ APIRouter.get('', tokenVerifier, async (req, res) => {
 APIRouter.post('/:id', tokenVerifier, async (req, res) => {
     try {
         if (!req.params.id) { //new friend id
-            res.status(400).send({ message: "Friend ID is not present." })
+            res.status(400).send({ message: "Friend ID is not present in the request." })
         }
         if (mongoose.isValidObjectId(req.user._id /*user id*/) && mongoose.isValidObjectId(req.params.id)) {
             let user = await User.findById(req.user._id);
@@ -35,12 +35,12 @@ APIRouter.post('/:id', tokenVerifier, async (req, res) => {
                 let newFriend = await User.findById(req.params.id);
                 //avoid adding same friend multiple times to an user, safety check
                 if (newFriend) {
-                    if (user.friends.includes(newFriend)) {
+                    if (user.friends.includes(req.params.id)) {
                         return res.status(400).send({ message: "Friend is already added to this user." });
                     }
-                    friendList.push(newFriend);
-                    await User.findByIdAndUpdate(id, { friends: friendList });
-                    res.status(201).send(`added user ${newFriend} to the friends of ${user.username}`);
+                    friendList.push(newFriend._id);
+                    await User.findByIdAndUpdate(req.user._id, { friends: friendList });
+                    res.status(201).send(`added user ${newFriend.username} to the friends of ${user.username}`);
                 } else re.status(404).send({ message: "friend not found" });
             } else res.status(404).send({ message: "user not found" });
         } else res.status(400).send({ message: "supplied user or friend ID is not valid" })
@@ -50,4 +50,35 @@ APIRouter.post('/:id', tokenVerifier, async (req, res) => {
     }
 });
 
+//removes user from friends list
+APIRouter.delete('/:id', tokenVerifier, async (req, res) => {
+    try {
+        if (!req.params.id) { //old friend id
+            res.status(400).send({ message: "Friend ID is not present in the request." })
+        }
+        if (mongoose.isValidObjectId(req.user._id /*user id*/) && mongoose.isValidObjectId(req.params.id)) {
+            let user = await User.findById(req.user._id);
+            if (user) {
+                let friendList = user.friends;
+                let oldFriend = await User.findById(req.params.id);
+                //safety check
+                if (oldFriend) {
+                    if (!user.friends.includes(req.params.id)) {
+                        return res.status(400).send({ message: "Friend is not in the user friend list." });
+                    } else {
+                        friendList = friendList.filter(function(item) {
+                            return item !== req.params.id
+                        });
+                    }
+                    await User.findByIdAndUpdate(req.user._id, { friends: friendList });
+                    res.status(201).send(`removed user ${oldFriend.username} to the friends of ${user.username}`);
+                } else re.status(404).send({ message: "friend not found" });
+            } else res.status(404).send({ message: "user not found" });
+        } else res.status(400).send({ message: "supplied user or friend ID is not valid" })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ message: error });
+    }
+});
 module.exports = APIRouter;
